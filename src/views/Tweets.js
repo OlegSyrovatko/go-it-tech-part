3efd016e8994f8df5befa00f50031e4b18c1f012
Oleg <112899765+OlegSyrovatko@users.noreply.github.com>
2023-06-02 17:19:28 +0300

    get filtered tweets

--- src/views/Tweets.js
+++ src/views/Tweets.js
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-// import { getFilter } from 'redux/selectors';
+import { getFilter } from 'redux/selectors';
 
 import { fetchTweets } from 'redux/tweets/operations';
-import { selectLoading } from 'redux/tweets/selectors';
-import { selectAllTweets } from 'redux/tweets/selectors';
+import { selectLoading, selectAllTweets, selectFollowers } from 'redux/tweets/selectors';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 
 import Filter from 'components/Filter';
@@ -21,13 +20,19 @@ import {
 
 
 const Tweets = () => {
-   const dispatch = useDispatch();
-  // const filter = useSelector(getFilter);
-  
-  const filteredTweets = useSelector(selectAllTweets);
-  // const filteredTweets = tweets.filter(contact =>
-  //   contact.name.toLowerCase().includes(lowerFilter)
-  // );
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
+  const tweets = useSelector(selectAllTweets);
+  const folowers = useSelector(selectFollowers);
+
+  let filteredTweets = tweets;
+
+  if (filter === "follow") {
+    filteredTweets = tweets.filter(tweet => !folowers.includes(Number(tweet.id)));
+  } else if (filter === "followings") {
+    filteredTweets = tweets.filter(tweet => folowers.includes(Number(tweet.id)));
+  } 
+    
   const isLoading = useSelector(selectLoading);
   useEffect(() => {
     if (isLoading) Loading.circle('Loading...')
@@ -39,28 +44,28 @@ const Tweets = () => {
     dispatch(fetchTweets());
   }, [dispatch]);
 
-return (
-  <>
-    <Book>
-      <Link to='/'>
-        <Button type="button" >
-          Back
-        </Button>
-      </Link><br /><br />
-      <Filter />
-      {filteredTweets.length > 0 && (
-        <UL>
-          {filteredTweets.map(tweet => (
-            <Li key={tweet.id}>
-              <TweetListItem tweet={tweet} />
-            </Li>
-          ))}
-        </UL>
-      )}
-    </Book>
-  </>
+  return (
+    <>
+      <Book>
+        <Link to='/'>
+          <Button type="button" >
+            Back
+          </Button>
+        </Link><br /><br />
+        <Filter />
+        {filteredTweets.length > 0 && (
+          <UL>
+            {filteredTweets.map(tweet => (
+              <Li key={tweet.id}>
+                <TweetListItem tweet={tweet} />
+              </Li>
+            ))}
+          </UL>
+        )}
+      </Book>
+    </>
 
- );
+  );
 };
 export default Tweets;
 
