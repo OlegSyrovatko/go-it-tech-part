609cc28883fc00b54c0dd9f36c3409f817d548d7
Oleg <sirov@ukr.net>
2023-06-02 23:20:59 +0300

    add load more button

--- src/views/Tweets.js
+++ src/views/Tweets.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getFilter } from 'redux/selectors';
@@ -18,54 +18,69 @@ import {
   // ContactsHead,
 } from './Tweets.styled';
 
-
 const Tweets = () => {
   const dispatch = useDispatch();
   const filter = useSelector(getFilter);
   const tweets = useSelector(selectAllTweets);
-  const folowers = useSelector(selectFollowers);
+  const followers = useSelector(selectFollowers);
+
+  const [visibleTweets, setVisibleTweets] = useState(3);
 
   let filteredTweets = tweets;
 
   if (filter === "follow") {
-    filteredTweets = tweets.filter(tweet => !folowers.includes(Number(tweet.id)));
+    filteredTweets = tweets.filter(tweet => !followers.includes(Number(tweet.id)));
   } else if (filter === "followings") {
-    filteredTweets = tweets.filter(tweet => folowers.includes(Number(tweet.id)));
-  } 
-    
+    filteredTweets = tweets.filter(tweet => followers.includes(Number(tweet.id)));
+  }
+
   const isLoading = useSelector(selectLoading);
+
   useEffect(() => {
-    if (isLoading) Loading.circle('Loading...')
-    else Loading.remove()
-      
+    if (isLoading) Loading.circle('Loading...');
+    else Loading.remove();
   }, [isLoading]);
-  
+
+    useEffect(() => {
+      setVisibleTweets(3);
+  }, [filter]);
+
   useEffect(() => {
     dispatch(fetchTweets());
   }, [dispatch]);
+
+  const handleLoadMore = () => {
+    setVisibleTweets(prevVisibleTweets => prevVisibleTweets + 3);
+  };
 
   return (
     <>
       <Book>
         <Link to='/'>
-          <Button type="button" >
+          <Button type="button">
             Back
           </Button>
         </Link><br /><br />
         <Filter />
         {filteredTweets.length > 0 && (
-          <UL>
-            {filteredTweets.map(tweet => (
-              <Li key={tweet.id}>
-                <TweetListItem tweet={tweet} />
-              </Li>
-            ))}
-          </UL>
+          <>
+            <UL>
+              {filteredTweets.slice(0, visibleTweets).map(tweet => (
+                <Li key={tweet.id}>
+                  <TweetListItem tweet={tweet} />
+                </Li>
+              ))}
+            </UL>
+            {filteredTweets.length > visibleTweets && (
+              <Button type="button" onClick={handleLoadMore}>
+                Load More
+              </Button>
+            )}
+          </>
         )}
       </Book>
     </>
-
   );
 };
-export default Tweets;
 
+export default Tweets;
